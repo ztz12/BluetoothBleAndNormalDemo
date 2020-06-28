edb7b2e831e7bf1d4d39ccb58cb7aab2bac7c5e4
zhangtianzhu <aq12jh@163.com>
2020-06-28 17:07:49 +0800

    加入扫描动画

--- app/src/main/java/win/lioil/bluetooth/ui/MainActivity.java
+++ app/src/main/java/win/lioil/bluetooth/ui/MainActivity.java
@@ -1,12 +1,7 @@
 package win.lioil.bluetooth.ui;
-
-import android.Manifest;
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.os.Build;
-import android.os.Bundle;
 import android.view.View;
 
 import win.lioil.bluetooth.app.APP;
@@ -16,12 +11,20 @@ import win.lioil.bluetooth.ble.BleServerActivity;
 import win.lioil.bluetooth.bt.BtClientActivity;
 import win.lioil.bluetooth.bt.BtServerActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initViews() {
+
+    }
+
+    @Override
+    public void initData() {
 
         // 检查蓝牙开关
         BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
@@ -43,19 +46,6 @@ public class MainActivity extends Activity {
             APP.toast("本机不支持低功耗蓝牙！", 0);
             finish();
             return;
-        }
-
-        // Android 6.0动态请求权限
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE
-                    , Manifest.permission.READ_EXTERNAL_STORAGE
-                    , Manifest.permission.ACCESS_COARSE_LOCATION};
-            for (String str : permissions) {
-                if (checkSelfPermission(str) != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(permissions, 111);
-                    break;
-                }
-            }
         }
     }
 
